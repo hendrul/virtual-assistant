2d1a08848f77c96e5b3f6840dc24abdff27de5b8
Raul Contreras <raul.contreras@factorit.com>
2019-02-16 14:55:35 -0500

    Logeo de peticiones

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
+var path = require("path");
 var express = require("express");
 var cors = require("cors");
 var bodyParser = require("body-parser");
+var morgan = require("morgan");
+var rfs = require("rotating-file-stream");
 var debug = require("debug")("botkit:webserver");
 
 var corsOptions = {
@@ -20,5 +23,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
 app.use(express.static(process.env.PUBLIC_PATH));
+
+// create a rotating write stream
+var accessLogStream = rfs(process.env.LOG_FILENAME || "request.log", {
+  interval: process.env.LOG_ROTATION || "1d", // rotate daily
+  rotationTime: true,
+  initialRotation: true,
+  path: path.resolve(__dirname, process.env.LOG_DIR || "log")
+});
+app.use(
+  morgan(process.env.LOG_FORMAT || "tiny", {
+    stream: accessLogStream
+  })
+);
 
 module.exports = app;
