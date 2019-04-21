d93506a96787f00703f0a4534c7275083ecee88e
Raul Contreras <raul.contreras@factorit.com>
2019-04-21 15:27:46 -0500

    Configura reescritura de url para cuando se añade una ruta base

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ var cors = require("cors");
 var bodyParser = require("body-parser");
 var morgan = require("morgan");
 var rfs = require("rotating-file-stream");
+var rewrite = require("express-urlrewrite");
 var debug = require("debug")("botkit:webserver");
 
 var corsOptions = {
@@ -22,19 +23,29 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
-app.use(express.static(process.env.PUBLIC_PATH));
+// Ignorar la ruta base, removerla del path
+process.env.BASE_PATH &&
+  app.use(
+    rewrite(
+      new RegExp(`^\/${process.env.BASE_PATH.replace(/^\/|\/$/, "")}(.*)$`),
+      "$1"
+    )
+  );
+process.env.PUBLIC_PATH && app.use(express.static(process.env.PUBLIC_PATH));
 
 // create a rotating write stream
-var accessLogStream = rfs(process.env.LOG_FILENAME || "request.log", {
-  interval: process.env.LOG_ROTATION || "1d", // rotate daily
-  rotationTime: true,
-  initialRotation: true,
-  path: path.resolve(__dirname, process.env.LOG_DIR || "log")
-});
-app.use(
-  morgan(process.env.LOG_FORMAT || "tiny", {
-    stream: accessLogStream
-  })
-);
+if (process.env.LOG_FILENAME) {
+  var accessLogStream = rfs(process.env.LOG_FILENAME, {
+    interval: process.env.LOG_ROTATION || "1d", // rotate daily
+    rotationTime: true,
+    initialRotation: true,
+    path: path.resolve(__dirname, process.env.LOG_DIR || "log")
+  });
+  app.use(
+    morgan(process.env.LOG_FORMAT || "tiny", {
+      stream: accessLogStream
+    })
+  );
+}
 
 module.exports = app;
