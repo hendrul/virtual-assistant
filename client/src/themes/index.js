cf5c46b062e247b60593faa9d3d9b0ed7f4f7ee4
Raul Contreras <raul.contreras@factorit.com>
2019-05-07 14:46:32 -0500

    Configuracion para sctr

--- client/src/themes/index.js
+++ client/src/themes/index.js
@@ -1,9 +1,9 @@
 // import default_theme from "./default_theme";
-// import onp_sctr from "./onp_sctr";
-import tailoy from "./tailoy";
+import onp_sctr from "./onp_sctr";
+// import tailoy from "./tailoy";
 
 export default {
   // default_theme
-  // onp_sctr
-  tailoy
+  onp_sctr
+  // tailoy
 };
